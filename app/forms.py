7b4766908aa1124b9885c155d6352e05348af999
mohamed kaid <mohamedkaid824@gmail.com>
2023-03-02 03:58:17 -0500

    week 5 and 6 hw

--- app/forms.py
+++ app/forms.py
@@ -13,3 +13,7 @@ class PokemonForm(FlaskForm):
     name = StringField('Name:', validators=[DataRequired()])
     submit_btn = SubmitField('Submit')
 
+class SignUpForm(FlaskForm):
+    email = EmailField('Email:', validators=[DataRequired()])
+    password = PasswordField('Password:', validators=[DataRequired()])
+    submit_btn = SubmitField('Sign Up')
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,24 @@
 from flask import render_template, request
 import requests
-from app.forms import LoginForm, PokemonForm
+from app.forms import PokemonForm, LoginForm, SignUpForm
 from app import app
 
 # ROUTES SECTION
 @app.route('/', methods=['GET'])
 def home():
     return render_template('home.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignUpForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        email = form.email.data.lower()
+        password = form.password.data
+        if email in app.config.get('REGISTERED_USERS') and password == app.config.get('REGISTERED_USERS').get(email).get('password'):
+            return f'Sign up Successful! Hello, {app.config.get("REGISTERED_USERS").get(email).get("name")}'
+        else:
+            return render_template('signup.html', form=form)
+    return render_template('signup.html', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -22,19 +34,17 @@ def login():
     return render_template('login.html', form=form)
 
 @app.route("/pokemon", methods=['GET','POST'])
-def pokemon_abilities():
+def pokemon():
     form = PokemonForm()
     print(request.method)
     if request.method == 'POST' and form.validate_on_submit():
-        name = form.names.data
+        name = form.name.data
         print(name)
         url = f'https://pokeapi.co/api/v2/pokemon/{name}'
         response = requests.get(url)
         if response.ok:
             pokemon_attributes = response.json()
-            new_pokemon_info = []
-            for pokemon in pokemon_attributes():
-                pokemon_info={
+            pokemon_info={
                     'pokemon_name':pokemon_attributes['name'],
                     'ability_name':pokemon_attributes['abilities'][0]['ability']['name'],
                     'base_experience':pokemon_attributes['base_experience'],
@@ -43,11 +53,11 @@ def pokemon_abilities():
                     'hp_stat':pokemon_attributes['stats'][0]['base_stat'],
                     'defense_stat':pokemon_attributes['stats'][0]['base_stat'],
                     }
-                new_pokemon_info.append(pokemon)
-                print(pokemon_info)
-        return render_template('pokemon.html', pokemon_info = pokemon_info, form=form)
-    else:
-        "This is an invalid option."
+            print(pokemon_info)
+            return render_template('pokemon.html', pokemon_info = pokemon_info, form=form)
+        else:
+            "This is an invalid option."
+            return render_template('pokemon.html', form=form)
     return render_template('pokemon.html', form=form)
 
 
