ca2b2d4fd07b6efab760ba6601ca4999469091b2
mohamed kaid <mohamedkaid824@gmail.com>
2023-05-06 00:53:48 -0400

    revisited

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-# CONFIG SECTION
+# CONFIG SECTIONS
 class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
