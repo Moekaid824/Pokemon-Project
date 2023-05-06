743c8a4b0af724e4f707eb3a8bffe1f9c46c486f
mohamed kaid <mohamedkaid824@gmail.com>
2023-05-06 01:04:04 -0400

    revisited

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_moment import Moment
 
-# Initializing Packages
+# Initialize Packages
 login = LoginManager()
 db = SQLAlchemy()
 migrate = Migrate()
